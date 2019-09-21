1d86e9ad73e0cb84a42876a5e44808fe53bd9143
Jie <linjie.newlife@gmail.com>
2019-09-21 15:02:59 -0400

    More on charity 4 homeless

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { NavigationDrawer } from 'react-md';
 import withFirebase from './firebase/withFirebase';
 import NavItemLink from './components/NavItemLink';
+import CharityRoutes from './routes/CharityRoutes';
 import Test from './pages/Test';
 import Home from './pages/Home';
-import Charity from './pages/Charity';
-import DiscoverCharity from './pages/DiscoverCharity';
-import CharityForHomeless from './pages/CharityForHomeless';
 
 const APP_NAME = 'TD Bank';
 
@@ -58,13 +56,7 @@ const App = () => {
         <Switch>
           <Route exact path="/test" component={Test} />
           <Route exact path="/" component={Home} />
-          <Route exact path="/charity" component={Charity} />
-          <Route exact path="/discover-charity" component={DiscoverCharity} />
-          <Route
-            exact
-            path="/charity4homeless"
-            component={CharityForHomeless}
-          />
+          <CharityRoutes />
         </Switch>
       </NavigationDrawer>
     </BrowserRouter>
--- src/App/pages/CharityForHomeless.js
+++ src/App/pages/CharityForHomeless.js
@@ -1,47 +1,44 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import CharityItem from '../components/CharityItem';
+import youthWithoutShelter from '../../assets/youth-without-shelter.jpg';
 
-const CharityForHomeless = () => {
-  const onClick = () => console.log();
+const buttonText = 'Learn more';
+
+const CharityForHomeless = ({ history }) => {
+  const onClick = () => history.push('/');
   return (
     <>
       <CharityItem
         title="Youth Without Shelter"
-        subtitle="homeless description"
-        imgSrc={''}
-        buttonText={'df'}
-        onClick={onClick}
-      />
-      <CharityItem
-        title="Youth Without Shelter"
-        subtitle="homeless description"
-        imgSrc={''}
-        buttonText={'df'}
+        subtitle="Youth Without Shelter (YWS) is an emergency residence and referral agency serving homeless youth. We are dedicated to providing shelter and support programs for homeless youth ages 16­-24."
+        imgSrc={youthWithoutShelter}
+        buttonText={buttonText}
         onClick={onClick}
       />
       <CharityItem
         title="Covenant House"
-        subtitle="homeless description"
-        imgSrc={''}
-        buttonText={'df'}
+        subtitle="Opened in 1982, Covenant House Toronto was the second international site established in the highly recognized childcare agency. The 28-site network spans from Alaska to Latin America."
+        imgSrc={youthWithoutShelter}
+        buttonText={buttonText}
         onClick={onClick}
       />
       <CharityItem
         title="Raising the Roof"
-        subtitle="homeless description"
-        imgSrc={''}
-        buttonText={'df'}
+        subtitle="Raising the Roof are a national leader in long-term solutions to ending homelessness. Each year, they support more than 50 community agencies across the country, and work together with partners in all sectors to address the issue."
+        imgSrc={youthWithoutShelter}
+        buttonText={buttonText}
         onClick={onClick}
       />
       <CharityItem
         title="Eva's Initiatives for Homeless Youth"
-        subtitle="homeless description"
-        imgSrc={''}
-        buttonText={'df'}
+        subtitle="Eva's Initatives for Homeless Youth focus on meeting young people’s immediate needs at the same time that we provide skills, training, and support to help them build brighter futures."
+        imgSrc={youthWithoutShelter}
+        buttonText={buttonText}
         onClick={onClick}
       />
     </>
   );
 };
 
-export default CharityForHomeless;
+export default withRouter(CharityForHomeless);
