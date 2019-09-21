e3252e10aba2267c59d78184ee728f30d54e5756
Jie <linjie.newlife@gmail.com>
2019-09-21 14:18:31 -0400

    build

---
+++ ui/precache-manifest.c675470fa6b8c98b3d41de8789c4c3d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9818f821d834da4fbfb8ef477aa3c925",
+    "url": "/static/media/homelessness.9818f821.jpg"
+  },
+  {
+    "revision": "f62119d417d14cb726eb39c3edae6cae",
+    "url": "/static/media/environment.f62119d4.jpg"
+  },
+  {
+    "revision": "70cea8504969fb065071cb008198cbec",
+    "url": "/static/media/diseases.70cea850.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd745b759ed7d1656e73",
+    "url": "/static/js/main.018d18d7.chunk.js"
+  },
+  {
+    "revision": "1bbdb46c5ff5ce59ab39",
+    "url": "/static/js/2.22b38422.chunk.js"
+  },
+  {
+    "revision": "dd745b759ed7d1656e73",
+    "url": "/static/css/main.82f0774e.chunk.css"
+  },
+  {
+    "revision": "47b6e94ac6466243483d1b8a870cec82",
+    "url": "/index.html"
+  }
+];
