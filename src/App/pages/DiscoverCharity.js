8b2a4de83770886d240ab7a4bcdf2e26b938b194
Jie <linjie.newlife@gmail.com>
2019-09-22 13:49:08 -0400

    More cancer

--- src/App/pages/DiscoverCharity.js
+++ src/App/pages/DiscoverCharity.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'react-router';
 import { Button, Media, TextField, FontIcon, List, ListItem } from 'react-md';
+import CharityItem from '../components/CharityItem';
 import youthWithoutShelter from '../../assets/youth-without-shelter.jpg';
+import cancerImg from '../../assets/cancer.jpg';
 
 const findCharities = 'Find your charities';
 
@@ -15,11 +17,34 @@ const StyledContainer = styled.div`
 `;
 
 const CancerList = ({ onClick }) => (
-  <List className="md-cell md-paper md-paper--1">
-    <ListItem primaryText="Prostate Cancer Canada" onClick={onClick} />
-    <ListItem primaryText="after BREAST CANCER" />
-    <ListItem primaryText="Pancreatic Cancer Canada Foundation" />
-  </List>
+  // <List className="md-cell md-paper md-paper--1">
+  //   <ListItem primaryText="Prostate Cancer Canada" onClick={onClick} />
+  //   <ListItem primaryText="after BREAST CANCER" />
+  //   <ListItem primaryText="Pancreatic Cancer Canada Foundation" />
+  // </List>
+  <>
+    <CharityItem
+      title="Prostate Cancer Canada"
+      subtitle="Prostate Cancer Canada is the leading national foundation dedicated to the elimination of the most common cancer in men through research."
+      onClick={onClick}
+      buttonText="add"
+      imgSrc={cancerImg}
+    />
+    <CharityItem
+      title="after BREAST CANCER"
+      subtitle="After Breast Cancer (ABC) is a registered charity that was founded in 2013 by Alicia Vianga, a certified bra and breast prosthesis fitter and owner of Premier Jour Fine Lingerie & Swimwear, a Toronto-based intimate apparel boutique."
+      onClick={onClick}
+      buttonText="add"
+      imgSrc={cancerImg}
+    />
+    <CharityItem
+      title="Pancreatic Cancer Canada Foundation"
+      subtitle="Pancreatic Cancer Canada Foundation (PCC) is Canada’s only foundation dedicated to raising money and awareness for pancreatic cancer. PCC supports research into early detection, and effective treatment options."
+      onClick={onClick}
+      buttonText="add"
+      imgSrc={cancerImg}
+    />
+  </>
 );
 
 const MyList = () => (
